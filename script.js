4cb6111de220bb5fca3b7531086f946edfc15ed0
ronaldo7810 <44244895+ronaldo7810@users.noreply.github.com>
2018-12-12 17:55:25 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#atlas").click(function() {
+$("#atlas").fadeToggle();
+});
+$("#cash").click(function() {
+$("#cash").fadeToggle();
+});
+$("#henley").click(function() {
+$("#henley").fadeToggle();
+});
